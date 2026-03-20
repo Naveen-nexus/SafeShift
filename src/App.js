41ddf386aa5c44723d2cc8f17b4ae4ec9d6ebbb2
Naveen-nexus <nk4451@srmist.edu.in>
2026-03-20 19:17:33 +0530

    Implement basic application shell layout

---
+++ src/App.js
@@ -0,0 +1,3 @@
+import React from 'react';
+function App() { return <div className='app-shell'><aside className='sidebar'></aside><main className='dashboard-main'></main></div>; }
+export default App;
