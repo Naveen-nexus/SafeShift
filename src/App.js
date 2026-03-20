0a1fe5b14740b17e7e1252783c9d3ce54f1affb1
Naveen-nexus <nk4451@srmist.edu.in>
2026-03-20 19:18:35 +0530

    Create action panel for event simulation

--- src/App.js
+++ src/App.js
@@ -1,3 +1,3 @@
 import React from 'react';
-function App() { return <div className='app-shell'><aside>SafeShift</aside><main><header>Dash</header><div className='stats'>...</div><div className='profile'>Partner: Naveen</div></main></div>; }
+function App() { return <div className='app-shell'><aside>SafeShift</aside><main><header>Dash</header><div className='stats'>...</div><div className='profile'>...</div><div className='actions'><button>Rain</button></div></main></div>; }
 export default App;
